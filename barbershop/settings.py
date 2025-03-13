c6c19e319d608e1a339e35fd14a5a348817ac9d8
VladimirMonin <vladimir.monin2016@yandex.ru>
2025-03-13 23:24:36 +0500

    lesson_41: добавили переменные окружения и .env

--- barbershop/settings.py
+++ barbershop/settings.py
@@ -11,6 +11,13 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+# Установили python dotenv и импортировали
+from dotenv import load_dotenv
+import os
+
+
+# Загрузили переменные окружения
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3_1cys5srztses#&*nlwan9f%i1p2#+oyr^e3z$ku)n%tj8%ve'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
