3cdff3218311fa6ab804f35e944ef668f10ac13e
VladimirMonin <vladimir.monin2016@yandex.ru>
2025-03-18 22:00:05 +0500

    feat: конвертер путей int и страничка master_detail

--- barbershop/urls.py
+++ barbershop/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from core.views import main
+from core.views import main, master_detail
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', main),
+    # Конвертер путей <int:master_id> преобразует часть URL в целое число и передаст его как аргумент в функцию master_detail
+    path('masters/<int:master_id>/', master_detail),
 ]
--- core/views.py
+++ core/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+masters = [
+    {"id": 1, "name": "Эльдар 'Бритва' Рязанов"},
+    {"id": 2, "name": "Зоя 'Ножницы' Космодемьянская"},
+    {"id": 3, "name": "Борис 'Фен' Пастернак"},
+    {"id": 4, "name": "Иннокентий 'Лак' Смоктуновский"},
+    {"id": 5, "name": "Раиса 'Бигуди' Горбачёва"},
+]
+
 
 def main(request):
     return HttpResponse("""
 <h1>Barbershop</h1>
 <p>Приветсвую путник. Ты находишься на сайте барбершопа. Здесь ты можешь записаться на стрижку, узнать цены и многое другое.</p>
 """)
+
+def master_detail(request, master_id):
+    try:
+        master = [m for m in masters if m['id'] == master_id][0]
+    except IndexError:
+        return HttpResponse("Мастера не найдено")
+    return HttpResponse(f"<h1>{master['name']}</h1>")
+    
