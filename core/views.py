52e5066fc9b323944b202f944536d0f4a766ec13
VladimirMonin <vladimir.monin2016@yandex.ru>
2025-03-18 21:40:22 +0500

    lesson_42: первый маршрут и первая view c HttpResponse

--- core/views.py
+++ core/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def main(request):
+    return HttpResponse("""
+<h1>Barbershop</h1>
+<p>Приветсвую путник. Ты находишься на сайте барбершопа. Здесь ты можешь записаться на стрижку, узнать цены и многое другое.</p>
+""")
