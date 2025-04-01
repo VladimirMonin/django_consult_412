faaf58a5cbc0fa0f542eb7f3557016c15b2c56e4
VladimirMonin <vladimir.monin2016@yandex.ru>
2025-04-01 23:52:57 +0700

    lesson_46: добавлены собственные теги шаблона для форматирования имени и должности

--- core/templatetags/price_extras.py
+++ core/templatetags/price_extras.py
@@ -14,3 +14,21 @@ def format_price(value, currency='₽'):
     except (ValueError, TypeError):
         # Если не удалось преобразовать в число, возвращаем исходное значение
         return value
+    
+
+# Мы можем сделать простой и инклюзивный тег для шаблонизатора
+@register.simple_tag(name='format_name')
+def format_name(name):
+    """Форматирует имя, делая первую букву заглавной и добавляет перед именем Мастер"""
+    if not name:
+        return ""
+    return f'Мастер {name.capitalize()}'
+
+
+# Мы можем сделать простой тег с параметрами для position
+@register.simple_tag(name='format_position')
+def format_position(position, param1, param2):
+    """Форматирует позицию, добавляя перед ней 'Должность:'"""
+    if not position:
+        return ""
+    return f'Должность: {position.capitalize()} {param1} {param2}'
