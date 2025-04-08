b35e34a1d1079136a840e6de792a99960cf4f5cf
VladimirMonin <vladimir.monin2016@yandex.ru>
2025-04-08 23:34:45 +0500

    feat: добавлены поля date_created и date_updated в модель Order, а также статус заказа с предустановленными значениями; создана миграция для обновления базы данных.

--- core/models.py
+++ core/models.py
@@ -11,9 +11,21 @@ JsonField - поле для хранения JSON-данных.
 
 
 class Order(models.Model):
-    # id - генерируется автоматически
+
+    # Статусы заказов
+    STATUS_CHOICES = [
+        ("not_approved", "Не подтвержден"),
+        ("moderated", "Прошел модерацию"),
+        ("spam", "Спам"),
+        ("approved", "Подтвержден"),
+        ("in_awaiting", "В ожидании"),
+        ("completed", "Завершен"),
+        ("canceled", "Отменен"),
+    ]
+
     client_name = models.CharField(max_length=100)
     services = models.CharField(max_length=200)
     master_id = models.IntegerField()
-    date = models.DateField()
-    status = models.CharField(max_length=50)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES, default="not_approved")
