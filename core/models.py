84155b9e7a411cab11bbcb9f4ddb5d0e309ff700
VladimirMonin <vladimir.monin2016@yandex.ru>
2025-04-10 23:01:27 +0500

    lesson_49: добавлена модель Service и связь многие ко многим с моделью Master; обновлены поля в модели Master и созданы миграции для изменения структуры базы данных.

--- core/models.py
+++ core/models.py
@@ -24,6 +24,7 @@ class Order(models.Model):
     status = models.CharField(max_length=50, choices=STATUS_CHOICES, default="not_approved")
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
+    # Один ко многим
     master = models.ForeignKey("Master", on_delete=models.SET_NULL, null=True, related_name="orders")
     appointment_date = models.DateTimeField(blank=True, null=True)
 
@@ -36,6 +37,15 @@ class Master(models.Model):
     address = models.CharField(max_length=255)
     email = models.EmailField(blank=True)
     experience = models.PositiveIntegerField()
+    # Многие ко многим
+    services = models.ManyToManyField("Service", related_name="masters")
     is_active = models.BooleanField(default=True)
 
 
+class Service(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    duration = models.PositiveIntegerField(help_text="Время в минутах") # help_text - это текст, который будет отображаться в админке
+    is_popular = models.BooleanField(default=False)
+    image = models.ImageField(upload_to="services/", blank=True, null=True)
