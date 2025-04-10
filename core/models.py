3a5a397696179eedb006ae112551a3a61478ad3f
VladimirMonin <vladimir.monin2016@yandex.ru>
2025-04-10 21:59:26 +0500

    lesson_49: добавлена модель Master и обновлены поля в модели Order; созданы миграции для изменения структуры базы данных.

--- core/models.py
+++ core/models.py
@@ -1,3 +1,4 @@
+from doctest import master
 from django.db import models
 """
 
@@ -18,9 +19,21 @@ class Order(models.Model):
     ]
 
     client_name = models.CharField(max_length=100)
-    services = models.CharField(max_length=200)
-    services_22 = models.CharField(max_length=200, blank=True)
-    master_id = models.IntegerField()
+    phone = models.CharField(max_length=20)
+    comment = models.TextField(blank=True)
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES, default="not_approved")
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=50, choices=STATUS_CHOICES, default="not_approved")
+    master = models.ForeignKey("Master", on_delete=models.SET_NULL, null=True)
+    appointment_date = models.DateTimeField(blank=True, null=True)
+
+
+class Master(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    photo = models.ImageField(upload_to="masters/")
+    phone = models.CharField(max_length=20)
+    address = models.CharField(max_length=255)
+    email = models.EmailField(blank=True)
+    experience = models.PositiveIntegerField()
+    is_active = models.BooleanField(default=True)
