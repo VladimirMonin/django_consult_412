a40f4b8e6da05928d404518118c5bcd002c9eeb6
VladimirMonin <vladimir.monin2016@yandex.ru>
2025-04-15 21:24:19 +0500

    lesson_50: удален неиспользуемый маршрут и класс Employee из views.py для упрощения кода

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,7 @@
 # core/urls.py
 from django.contrib import admin
 from django.urls import path
-from .views import master_detail, thanks, test, orders_list, order_detail
+from .views import master_detail, thanks, orders_list, order_detail
 # Эти маршруты будут доступны с префиксом /barbershop/
 
 urlpatterns = [
@@ -9,7 +9,5 @@ urlpatterns = [
     path('thanks/', thanks, name='thanks'),
     path('orders/', orders_list, name='orders_list'),
     path('orders/<int:order_id>/', order_detail, name='order_detail'),
-
-    path('test/', test),
 
 ]
--- core/views.py
+++ core/views.py
@@ -33,88 +33,13 @@ def thanks(request):
     return render(request, "core/thanks.html", context)
 
 
-class Employee:
-    def __init__(
-        self,
-        name: str,
-        is_active: bool,
-        is_married: bool,
-        age: int,
-        salary: float,
-        position: str,
-        hobbies: list,
-    ):
-        self.name = name
-        self.is_active = is_active
-        self.is_married = is_married
-        self.age = age
-        self.salary = salary
-        self.position = position
-        self.hobbies = hobbies
-
-    def __str__(self):
-        return f"Имя: {self.name}.\nВозраст: {self.age}.\nЗарплата: {self.salary}.\nДолжность: {self.position}."
-
-
-def test(request):
-
-    employee = Employee(
-        "Алевтина",
-        True,
-        True,
-        42,
-        100000,
-        "manager",
-        ["Журналы про усы", "Компьютерные игры", "Пиво"],
-    )
-    employee2 = Employee(
-        "Бородач",
-        True,
-        False,
-        25,
-        50000,
-        "master",
-        ["Садоводство", "Пиво", "Компьютерные игры"],
-    )
-    employee3 = Employee(
-        "Барбарис",
-        True,
-        False,
-        30,
-        60000,
-        "master",
-        ["Газонокосилки", "Пиво", "Стрельба из арбалета"],
-    )
-    employee4 = Employee(
-        "Сифон",
-        True,
-        True,
-        35,
-        70000,
-        "master",
-        ["Брендовый шмот", "Походы в ГУМ", "Аниме"],
-    )
-
-    # Список сотрудников
-    employees = [employee, employee2, employee3, employee4]
-
-    context = {
-        "string": "Мастер по усам",
-        "number": 42,
-        "list": ["Стрижка бороды", "Усы-таракан", "Укладка бровей"],
-        "dict": {"best_master": "Алевтина Арбузова"},
-        "employee": employee,
-        "employee2": employee2,
-        "employees": employees,
-    }
-    return render(request, "test.html", context)
-
 @login_required
 def orders_list(request):
     orders = Order.objects.all()
 
     context = {"orders": orders, "title": "Список заказов"}
     return render(request, "core/orders_list.html", context)
+
 
 @login_required
 def order_detail(request, order_id: int):
