e65cffdb96d6de58ad728d33529914cf29e248be
VladimirMonin <vladimir.monin2016@yandex.ru>
2025-04-29 23:01:18 +0500

    lesson_54: urlpatterns и MEDIA_URL раздача медиа в режиме разработчика

--- barbershop/urls.py
+++ barbershop/urls.py
@@ -4,18 +4,21 @@ from django.urls import path
 from core.views import landing
 from django.urls import include
 from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', landing, name='landing'),
+    path("admin/", admin.site.urls),
+    path("", landing, name="landing"),
     # Подключаем маршруты из приложения core
-    path('barbershop/', include('core.urls')),
-    
-
+    path("barbershop/", include("core.urls")),
 ]
 
 if settings.DEBUG:
     import debug_toolbar
+
     urlpatterns = [
-        path('__debug__/', include(debug_toolbar.urls)),
+        path("__debug__/", include(debug_toolbar.urls)),
     ] + urlpatterns
+
+    # Добавляем обслуживание медиа-файлов в режиме разработки
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
