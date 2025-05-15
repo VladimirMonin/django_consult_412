4b288f126a4dd3cdc1095967f32b83088ae4ae6a
VladimirMonin <vladimir.monin2016@yandex.ru>
2025-05-15 19:12:27 +0500

    feat: добавлена регистрация модели отзыва в админке для управления отзывами клиентов о мастерах

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Order, Master, Service
+from .models import Order, Master, Service, Review
 
 # Регистрация в одну строку
 admin.site.register(Order)
 admin.site.register(Master)
 admin.site.register(Service)
+admin.site.register(Review)
