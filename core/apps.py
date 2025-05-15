fb1564bf4f1aae9d68a1089d0572199506a2d1b2
VladimirMonin <vladimir.monin2016@yandex.ru>
2025-05-15 22:38:30 +0500

    lesson_56: добавлен сигнал для проверки текста отзыва и автоматической публикации при отсутствии негативных слов (учебный пример)

--- core/apps.py
+++ core/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
+
+    # метод ready вызывается при запуске приложения и мы можем например импортировать сигналы
+    def ready(self):
+        # Импортируем сигналы, чтобы они были зарегистрированы при запуске приложения
+        import core.signals
---
+++ core/signals.py
@@ -0,0 +1,23 @@
+# Опишем сигнал, который будет слушать создание записи в модель Review и проверять есть ли в поле text слова "плохо" или "ужасно". - Если нет, то меняем is_published на True
+
+from .models import Review
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+@receiver(post_save, sender=Review)
+def check_review_text(sender, instance, created, **kwargs):
+    """
+    Проверяет текст отзыва на наличие слов "плохо" или "ужасно".
+    Если таких слов нет, то устанавливает is_published в True.
+    """
+    if created:
+        if "плохо" not in instance.text.lower() and "ужасно" not in instance.text.lower():
+            instance.is_published = True
+            instance.save()
+            # Вывод в терминал 
+            print(f"Отзыв '{instance.client_name}' опубликован автоматически.")
+        else:
+            instance.is_published = False
+            instance.save()
+            # Вывод в терминал 
+            print(f"Отзыв '{instance.client_name}' не опубликован из-за негативных слов.")
