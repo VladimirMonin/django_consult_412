728a7aec0108028e9a13aecebe0f08b8feb5a018
VladimirMonin <vladimir.monin2016@yandex.ru>
2025-05-20 21:49:33 +0500

    lesson_57: простой сигнал с телеграм ботом при создании заявки на услугу

--- core/signals.py
+++ core/signals.py
@@ -1,9 +1,17 @@
 # Опишем сигнал, который будет слушать создание записи в модель Review и проверять есть ли в поле text слова "плохо" или "ужасно". - Если нет, то меняем is_published на True
 
-from .models import Review
+from .models import Order, Review
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .mistral import moderate_review
+# Импорт всего что нужно для работы бота
+from .telegram_bot import send_telegram_message
+from asyncio import run
+# Из настроек импортируем токен и id чата
+from django.conf import settings
+
+TELEGRAM_BOT_API_KEY = settings.TELEGRAM_BOT_API_KEY
+TELEGRAM_USER_ID = settings.TELEGRAM_USER_ID
 
 @receiver(post_save, sender=Review)
 def check_review_text(sender, instance, created, **kwargs):
@@ -22,3 +30,27 @@ def check_review_text(sender, instance, created, **kwargs):
             instance.save()
             # Вывод в терминал 
             print(f"Отзыв '{instance.client_name}' не опубликован из-за негативных слов.")
+
+
+@receiver(post_save, sender=Order)
+def telegram_order_notification(sender, instance, created, **kwargs):
+    if created:
+        # Если заказ создан, добываем данные 
+        client_name = instance.client_name
+        phone = instance.phone
+        comment = instance.comment
+
+        # Формируем сообщение
+        telegram_message = f"""
+*Новый заказ!*
+
+*Имя клиента:* {client_name}
+*Телефон:* {phone}
+*Комментарий:* {comment}
+*Ссылка на заказ:* http://127.0.0.1:8000/admin/core/order/{instance.id}/change/
+
+=========================
+"""
+        # Логика отправки сообщения в Telegram
+        run(send_telegram_message(TELEGRAM_BOT_API_KEY, TELEGRAM_USER_ID, telegram_message))
+
