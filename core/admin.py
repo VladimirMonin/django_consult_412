047b1b72278e875ca4f28426c66b398d51123331
VladimirMonin <vladimir.monin2016@yandex.ru>
2025-05-22 21:17:11 +0500

    lesson_58: руссификация названия приложения в админке и настройка отображения MasterAdmin в admin.py

--- core/admin.py
+++ core/admin.py
@@ -3,6 +3,24 @@ from .models import Order, Master, Service, Review
 
 # Регистрация в одну строку
 admin.site.register(Order)
-admin.site.register(Master)
 admin.site.register(Service)
 admin.site.register(Review)
+
+# Создание кастомной админки для модели Master
+
+
+class MasterAdmin(admin.ModelAdmin):
+    # Список полей, которые будут отображаться в админке в виде таблицы (в этом же порядке!)
+    list_display = ("first_name", "last_name", "phone", "experience", "is_active")
+    # Кликабельные поля - имя и фамилия мастера
+    list_display_links = ("first_name", "last_name")
+    # Фильтры которые Django сделает автоматически сбоку
+    list_filter = ("is_active", "services", "experience")
+    # Поля, по которым можно будет искать
+    search_fields = ("first_name", "last_name", "phone")
+    # Порядок сортировки
+    ordering = ("last_name", "first_name")
+
+
+# Регистрация модели Master с кастомной админкой
+admin.site.register(Master, MasterAdmin)
--- core/apps.py
+++ core/apps.py
@@ -4,6 +4,7 @@ from django.apps import AppConfig
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
+    verbose_name = 'Барбершоп "Арбуз"'  # Название приложения в админке
 
     # метод ready вызывается при запуске приложения и мы можем например импортировать сигналы
     def ready(self):
