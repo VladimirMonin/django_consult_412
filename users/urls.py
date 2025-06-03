8f413c53b947446e77156b097063d5ae49fe4977
VladimirMonin <vladimir.monin2016@yandex.ru>
2025-06-03 15:44:12 +0500

    fix: исправлена ошибка NoReverseMatch для profile_edit и опечатка в as_view

--- users/urls.py
+++ users/urls.py
@@ -12,4 +12,6 @@ urlpatterns = [
     path('profile/<int:pk>/', views.UserProfileDetailView.as_view(), name='profile_detail'),
     # Обновленный маршрут для смены пароля
     path('password_change/', views.UserPasswordChangeView.as_view(), name='password_change'),
+    # Новый маршрут для редактирования профиля
+    path('profile/edit/', views.UserProfileUpdateView.as_view(), name='profile_edit'),
 ]
