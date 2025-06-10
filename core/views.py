3d4e822aa5119e233f64fbb981a52f04bceb3f52
VladimirMonin <vladimir.monin2016@yandex.ru>
2025-06-10 22:13:25 +0500

    lesson_63: ServiceUpdateView в конечном виде

--- core/views.py
+++ core/views.py
@@ -344,9 +344,21 @@ class ServiceUpdateView(UpdateView):
     template_name = "core/service_form.html"
     success_url = reverse_lazy("services_list")
     extra_context = {
-        "title": f'Редактирование услуги',
         "button_txt": "Обновить",
     }
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = f'Редактирование услуги: {self.object.name}'
+        return context
+    
+    def form_valid(self, form):
+        messages.success(self.request, f"Услуга '{form.cleaned_data['name']}' успешно обновлена!")
+        return super().form_valid(form)
+    
+    def form_invalid(self, form):
+        messages.error(self.request, "Ошибка формы: проверьте ввод данных.")
+        return super().form_invalid(form)
 
 
 class ServiceCreateView(CreateView):
