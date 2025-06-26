020d2afbdc154179d8561df11f1196e7a563bd42
VladimirMonin <vladimir.monin2016@yandex.ru>
2025-06-26 23:24:01 +0500

    lesson_67: кеширование в шаблоне и во view

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,21 @@
 from .models import Post, Comment, Category, Tag
 from django.views.generic import ListView, DetailView
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 
-
+@method_decorator(cache_page(60 *1), name='get') # Кешируем страницу на 1 минуту
 class PostsListView(ListView):
     model = Post
     template_name = 'blog/posts_list.html'
     context_object_name = 'posts'
     paginate_by = 2
+
+    # Оптимизируем запрос - сделаем жадный запрос Категории теги комменты расширение метода get_queryset
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.select_related('category', 'author')
+        queryset = queryset.prefetch_related('tags', 'comments')
+        return queryset
 
 
 class PostDetailView(DetailView):
