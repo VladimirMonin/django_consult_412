766bdf3ea1d29f4dda02301450efe86a3e39b058
VladimirMonin <vladimir.monin2016@yandex.ru>
2025-07-08 21:40:14 +0500

    lesson_70: - Вынесли настройку DEBUG для запуска Django сервера в .env - Обновили секреты на GitHub - Обновили .env.example

--- barbershop/settings.py
+++ barbershop/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+DEBUG = os.getenv("DEBUG_MODE", "True") == "True" 
 # Добавляем разрешенные хосты для нашего домена и IP
 ALLOWED_HOSTS = [
     "92.255.76.225",  # Публичный IP
